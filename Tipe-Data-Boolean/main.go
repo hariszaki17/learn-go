8916e88dd36139de74ee06581e585d818752f830
hariszaki17 <hariszaki17@gmail.com>
2021-02-01 10:53:56 +0700

    learn datatype boolean

---
+++ Tipe-Data-Boolean/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Benar =", true) // Benar = true
+	fmt.Println("Salah =", false) // Salah = true
+}
+
+/* 
+===NOTE===
+- boolean pada go menggunakan kata kunci bool
+- nilai boolean: true atau false
+*/
